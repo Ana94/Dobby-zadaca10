47dd89460f69f3fe5c6d79d4c1e3e4ac83d92858
Ana Leh <ana.leh05@gmail.com>
2015-03-23 19:02:15 +0100

    Deseta zadaća na kolegiju Oblikovanje mrežnih stranica
    Za desetu zadaću imali smo zadatak napraviti web stranicu sa 3
    javascript naredbe

---
+++ skripta.js
@@ -0,0 +1,29 @@
+﻿function free() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("bulbon")) {
+        image.src = "dobby1.jpg";
+    } else {
+        image.src = "dobby.gif";
+    }
+}
+
+function dopisivanje() {
+    var person = prompt("Misliš li da je Dobby dobar?", "da");
+    
+    if (person != "ne") {
+        document.getElementById("demo").innerHTML =
+        "Dobby has heard of your greatness, sir, but of your goodness, Dobby never knew...";
+	}	
+	 if (person != "da") {
+        document.getElementById("demo").innerHTML =
+        "Bad Dobby! Bad Dobby!";
+    }
+}
+
+
+function dobby() {
+	var prva= prompt ("Vilenjak", "Dobby")
+	var druga= prompt ("Tvoje ime")
+	alert(prva + " i " + druga + " mogu biti prijatelji " + Math.random()*100 + " % ");
+}
+
